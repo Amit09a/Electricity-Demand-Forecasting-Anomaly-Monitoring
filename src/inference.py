7f35c3aeb3ae48145767cff58d3db9d37fa9f7f6
Amit09a <amitm4148@gmail.com>
2026-02-10 00:46:41 +0530

    Update inference.py

--- src/inference.py
+++ src/inference.py
@@ -3,7 +3,7 @@ import json
 import pandas as pd
 import os
 
-# Get project root (one level above src)
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 ARTIFACTS_PATH = os.path.join(BASE_DIR, "artifacts")
